739f933f85650a492ef3a74d74a483cde0c648a1
sherpajun <jun1781@naver.com>
2021-11-09 23:33:58 +0900

    Update App.js
    how to use flex:)

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello i create this app</Text>
-      
-      <Text style={styles.text}>what is u r first App</Text>
-      
-      <Text style={styles.text}>what should pratice{"\n"} coding or get a job</Text>
-      <StatusBar style="auto" />
+    <View style={{flexDirection:"row",flex:"1"}}>/*부모가 가장중요하다*/
+      <View style={{flex:"1",backgroundColor:"teal"}}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    fontSize:30,
-    color: 'red',
-  }
-});
