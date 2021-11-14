2a1b9cf302a7bfbe8041a15177287cf5a7d2ec39
sherpajun <jun1781@naver.com>
2021-11-14 18:34:58 +0900

    Update App.js
    Text color , View CSS

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
 
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {View } from 'react-native';
+import {View,StyleSheet,Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flexDirection:"row",flex:"1"}}>/*부모가 가장중요하다*/
-      <View style={{flex:"1",backgroundColor:"teal"}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>30</Text>
+          <Text style={styles.description}>판단기준</Text>
+        </View>
+      </View>
+      <StatusBar style="light"/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor:"#F5DEB3",
+  },
+  city:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  cityName:{
+    color:"#0099a4",
+    fontSize:68,
+    fontWeight:"500",
+
+  },
+  weather:{
+    flex:3,
+    backgroundColor:"#E9967A",
+  },
+
+  day:{
+    flex:1,
+    backgroundColor:"#4682B4",
+    alignItems:"center",
+  },
+  temp:{
+    fontSize:178,
+    marginTop:50,
+    color:"#FFFFFF",
+    
+
+  },
+  description:{
+    fontSize:60,
+    marginTop:-30,
+    color:"#FFFFFF",
+  }
+
+
+})
 
